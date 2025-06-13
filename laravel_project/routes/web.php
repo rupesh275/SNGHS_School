057295fe1344128cbe30c024f7f401a64fd6894d
Tezits <tezitservices@gmail.com>
2025-06-13 12:19:32 +0530

    Installed Laravel Breeze

--- laravel_project/routes/web.php
+++ laravel_project/routes/web.php
@@ -1,6 +1,24 @@
 <?php
 
-use App\Http\Controllers\Login;
-use App\Http\Controllers\Home;
-Route::get('dashboard', [Home::class, 'index']);
-Route::get('login', [Login::class, 'login']);
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
